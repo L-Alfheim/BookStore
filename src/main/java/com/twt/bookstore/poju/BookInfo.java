16cd62ff010c0d3bb98ea68601683bb3edc50d06
Alfheim <17310881@qq.com>
2025-12-16 15:16:34 +0800

    增加info_books的update_at字段 增加BookInfo poju类的构造信息 修正updated，created这类ed问题

--- src/main/java/com/twt/bookstore/poju/BookInfo.java
+++ src/main/java/com/twt/bookstore/poju/BookInfo.java
@@ -28,6 +28,7 @@ public class BookInfo {
     private boolean isAvailable; //在售状态
     private boolean isDeleted;   //删除状态
     private Instant createTime;   //上架时间
+    private Instant updateTime; //更新时间
     private Instant deleteTime;   //删除时间
 
     /**
@@ -36,5 +37,35 @@ public class BookInfo {
      */
     public BookInfo(UUID uuid) {
         this.uuid = uuid;
+    }
+
+    /**
+     * 这个构造器来自数据库字段更新后报错
+     * 是缺省updateTime的构造器
+     * @param id
+     * @param uuid
+     * @param title
+     * @param author
+     * @param price
+     * @param description
+     * @param stockQuantity
+     * @param isAvailable
+     * @param isDeleted
+     * @param createTime
+     * @param deleteTime
+     */
+    public BookInfo(Long id, UUID uuid, String title, String author, BigDecimal price, String description,
+            Integer stockQuantity, boolean isAvailable, boolean isDeleted, Instant createTime, Instant deleteTime) {
+        this.id = id;
+        this.uuid = uuid;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.description = description;
+        this.stockQuantity = stockQuantity;
+        this.isAvailable = isAvailable;
+        this.isDeleted = isDeleted;
+        this.createTime = createTime;
+        this.deleteTime = deleteTime;
     }
 }
